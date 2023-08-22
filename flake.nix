50a07490aa191756296d20e5326e2b2cb79cce30
Turmaxx <88465473+Turmaxx@users.noreply.github.com>
2023-08-22 16:40:11 +0300

    formatting mi flakes
    nix lsp formatting doing its things, also changed inputs to a one liner.

--- flake.nix
+++ flake.nix
@@ -1,27 +1,27 @@
 {
   description = "Personal Homepage & Blog | Brook Seyoum";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    # system = "x86_64-darwin;"
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    devShells.${system}.default =
-      pkgs.mkShell
-        {
-          buildInputs = with pkgs; [
-            hugo
-          ];
-
-          shellHook = ''
-            hugo version
-          '';
-        };
-  };
+    let
+      system = "x86_64-linux";
+      # system = "x86_64-darwin;"
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default =
+        pkgs.mkShell
+          {
+            buildInputs = with pkgs; [
+              hugo
+            ];
+            shellHook = ''
+              hugo version
+            '';
+          };
+      app.serve.program = "${pkgs.writeShellScript "serve" ''
+        make serve
+      ''}";
+    };
 }
